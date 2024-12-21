a845d7b01f4e74abab19b76629f6d21d97599c50
aman-maharshi <amanm4harshi@gmail.com>
2024-12-21 19:22:13 +0530

    minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,31 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 const App = () => {
+ 
   return (
     <>
-      <header className='wrapper header'>
+      <header className="wrapper header">
         <a href="#features">Features</a>
         <a href="#pricing">Pricing</a>
         <a href="#faq">Faq</a>
         <a href="#contact">Contact</a>
       </header>
 
-      <section className='section'>
+      <section className='section hero'>
       </section>
 
-      <section id="features" className='section features'>
+      <section id="features" className='section'>
         <h1 className="section-title">Features</h1>
       </section>
-      <section id="pricing" className='section'>
+
+      <section id="pricing" className='section pricing'>
         <h1 className="section-title">Pricing</h1>
       </section>
-      <section id="faq" className='section faq'>
+
+      <section id="faq" className='section'>
         <h1 className="section-title">FAQ</h1>
       </section>
+      
       <section id="contact" className='section'>
         <h1 className="section-title">Contact</h1>
       </section>
