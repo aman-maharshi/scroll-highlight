22629c810152109ea82f5a34e3a614ecb09402dd
aman-maharshi <amanm4harshi@gmail.com>
2024-12-21 19:02:27 +0530

    minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -4,24 +4,24 @@ const App = () => {
   return (
     <>
       <header className='wrapper header'>
-        <a href="/">Features</a>
-        <a href="/">Pricing</a>
-        <a href="/">Faq</a>
-        <a href="/">Contact</a>
+        <a href="#features">Features</a>
+        <a href="#pricing">Pricing</a>
+        <a href="#faq">Faq</a>
+        <a href="#contact">Contact</a>
       </header>
       <section className='section'>
       </section>
       <section className='section features'>
-        <h1 className="section-title">Features</h1>
+        <h1 className="section-title" id="features">Features</h1>
       </section>
       <section className='section'>
-        <h1 className="section-title">Pricing</h1>
+        <h1 className="section-title" id="pricing">Pricing</h1>
       </section>
       <section className='section faq'>
-        <h1 className="section-title">FAQ</h1>
+        <h1 className="section-title" id="faq">FAQ</h1>
       </section>
       <section className='section'>
-        <h1 className="section-title">Contact</h1>
+        <h1 className="section-title" id="contact">Contact</h1>
       </section>
     </>
   )
