77eaf27b879fda4f79cd556e2e12c4a2b192c6a1
aman-maharshi <amanm4harshi@gmail.com>
2024-12-27 00:49:57 +0530

    highlight nav on scroll added

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react'
+import { Link as ScrollLink } from "react-scroll"
 
 const App = () => {
   const [hasScrolled, setHasScrolled] = useState(false)
@@ -14,33 +15,63 @@ const App = () => {
     }
   }, [])
 
-
-
   return (
     <>
       <header className={`wrapper header ${hasScrolled ? "scrolled" : ""}`}>
-        <a href="#features">Features</a>
-        <a href="#pricing">Pricing</a>
-        <a href="#faq">Faq</a>
-        <a href="#contact">Contact</a>
+        <ScrollLink
+          to="features"
+          spy
+          smooth
+          activeClass='link-active'
+          offset={-100}
+        >
+          Features
+        </ScrollLink>
+        <ScrollLink
+          to="pricing"
+          spy
+          smooth
+          activeClass='link-active'
+          offset={-100}
+        >
+          Pricing
+        </ScrollLink>
+        <ScrollLink
+          to="faq"
+          spy
+          smooth
+          activeClass='link-active'
+          offset={-100}
+        >
+          Faq
+        </ScrollLink>
+        <ScrollLink
+          to="contact"
+          spy
+          smooth
+          activeClass='link-active'
+          offset={-100}
+        >
+          Contact
+        </ScrollLink>
       </header>
 
-      <section className='section hero'>
+      <section className='section hero blue-bg'>
       </section>
 
-      <section id="features" className='section'>
+      <section name="features" className='section'>
         <h1 className="section-title">Features</h1>
       </section>
 
-      <section id="pricing" className='section pricing'>
+      <section name="pricing" className='section blue-bg'>
         <h1 className="section-title">Pricing</h1>
       </section>
 
-      <section id="faq" className='section'>
+      <section name="faq" className='section'>
         <h1 className="section-title">FAQ</h1>
       </section>
 
-      <section id="contact" className='section pricing'>
+      <section name="contact" className='section blue-bg'>
         <h1 className="section-title">Contact</h1>
       </section>
     </>
